4d9b3252a9138b238cc96ea11b761d75ff48160b
bininc <bininc@163.com>
2019-09-01 22:56:20 +0800

    2019.9.1

---
+++ Common/Web/WebHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace CommonLib.Web
+{
+    public class WebHelper
+    {
+
+    }
+}
