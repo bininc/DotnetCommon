b18140d8be8ca923c9c92dc16252c937cc978f50
Bininc <bininc@163.com>
2020-05-13 18:15:37 +0800

    修改目录层次架构

---
+++ Common/TestCommon/DateTimeHelperTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace TestCommon
+{
+    public class DateTimeHelperTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
